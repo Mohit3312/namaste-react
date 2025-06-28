f11ff3cc2a9192504236938327ef3f8cb2c49749
Mohit Kumar <mohit3312@gmail.com>
2025-06-29 00:27:53 +0530

    Understanding of JSX and Babel.

--- App.js
+++ App.js
@@ -1,44 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const reactHeader = React.createElement(
-  "div",
-  { id: "header" },
-  React.createElement("h1", {}, "I am header")
+// React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+// Creating react element using React
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//   },
+//   "Namaste React"
+// );
+// console.log(heading);
+
+// JSX - HTML-like or XML-like syntax
+// JSX - (transpiled before it reaches the JS Engine) - PARCEL - Babel
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+// Creating react element using JSX
+const jsxHeading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
 );
-
-{
-  /* 
-    <div id="parent">
-      <div id="child">
-         <h1>I'm h1 tag</h1>
-         <h2>I'm h2 tag</h2>
-      </div>
-    </div>; 
-    <div id="parent">
-      <div id="child2">
-         <h1>I'm h1 tag</h1>
-         <h2>I'm h2 tag</h2>
-      </div>
-    </div>; 
-  */
-}
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React"),
-    React.createElement("h2", {}, "by Akshay Saini"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
-
-console.log(parent); // object
-
-const header = ReactDOM.createRoot(document.getElementById("header"));
-header.render(reactHeader);
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(jsxHeading);
