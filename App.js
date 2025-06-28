f4e7f4aa9590333197413d5f57df413efd742261
Mohit Kumar <mohit3312@gmail.com>
2025-06-29 01:20:11 +0530

    Created React Component.

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-// Creating react element using React
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     id: "heading",
-//   },
-//   "Namaste React"
-// );
-// console.log(heading);
-
-// JSX - HTML-like or XML-like syntax
-// JSX - (transpiled before it reaches the JS Engine) - PARCEL - Babel
-// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-// Creating react element using JSX
-const jsxHeading = (
+const Title = () => (
   <h1 className="head" tabIndex="5">
     Namaste React using JSX
   </h1>
 );
-console.log(jsxHeading);
+
+// Component Composition
+// React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
