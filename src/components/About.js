5acbd42e60735a6c7ea39dcb8226457154e35773
Mohit Kumar <mohit3312@gmail.com>
2025-07-29 14:14:37 +0530

    Created class-based component with constructor, render, and componentDidMount methods

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,46 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is Namaste React Web Series</h2>
-    </div>
-  );
-};
+import { Component } from "react";
+import UserClass from "./UserClass";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass name={"First"} location={"Dehradun Class"} />
+        <UserClass name={"Second"} location={"US"} />
+      </div>
+    );
+  }
+}
 
 export default About;
+
+/*
+ - Parent Constructor
+ - Parent Render
+
+   - First Constructor
+   - First Render
+
+   - Second Constructor
+   - Second Render
+
+   <DOM UPDATED - IN SINGLE BATCH>
+   
+   - First Component Did Mount
+   - Second Component Did Mount
+
+ - Parent Component Did Mount 
+*/
